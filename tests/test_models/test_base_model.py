1a571da4337237e1b60d65bd0d345d7c8df12034
fady-helal <fadyhelal78>
2024-03-10 23:00:38 +0200

    Executable!

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-"""Defines unittests for models/base_model.py.
-
-Unittest classes:
-    TestBaseModel_instantiation
-    TestBaseModel_save
-    TestBaseModel_to_dict
+"""Defines unittests for base_model.py
 """
 import os
 import models
@@ -14,56 +9,56 @@ from time import sleep
 from models.base_model import BaseModel
 
 
-class TestBaseModel_instantiation(unittest.TestCase):
+class TestBaseModel(unittest.TestCase):
     """Unittests for testing instantiation of the BaseModel class."""
 
-    def test_no_args_instantiates(self):
+    def test_type_BaseModel(self):
         self.assertEqual(BaseModel, type(BaseModel()))
 
     def test_new_instance_stored_in_objects(self):
         self.assertIn(BaseModel(), models.storage.all().values())
 
-    def test_id_is_public_str(self):
+    def test_id(self):
         self.assertEqual(str, type(BaseModel().id))
 
-    def test_created_at_is_public_datetime(self):
+    def test_created_at(self):
         self.assertEqual(datetime, type(BaseModel().created_at))
 
-    def test_updated_at_is_public_datetime(self):
+    def test_updated_at(self):
         self.assertEqual(datetime, type(BaseModel().updated_at))
 
     def test_two_models_unique_ids(self):
-        bm1 = BaseModel()
-        bm2 = BaseModel()
-        self.assertNotEqual(bm1.id, bm2.id)
+        user1 = BaseModel()
+        user2 = BaseModel()
+        self.assertNotEqual(user1.id, user2.id)
 
     def test_two_models_different_created_at(self):
-        bm1 = BaseModel()
-        sleep(0.05)
-        bm2 = BaseModel()
-        self.assertLess(bm1.created_at, bm2.created_at)
+        user1 = BaseModel()
+        sleep(0.01)
+        user2 = BaseModel()
+        self.assertLess(user1.created_at, user2.created_at)
 
     def test_two_models_different_updated_at(self):
-        bm1 = BaseModel()
-        sleep(0.05)
-        bm2 = BaseModel()
-        self.assertLess(bm1.updated_at, bm2.updated_at)
+        user1 = BaseModel()
+        sleep(0.01)
+        user2 = BaseModel()
+        self.assertLess(user1.updated_at, user2.updated_at)
 
     def test_str_representation(self):
-        dt = datetime.today()
-        dt_repr = repr(dt)
-        bm = BaseModel()
-        bm.id = "123456"
-        bm.created_at = bm.updated_at = dt
-        bmstr = bm.__str__()
-        self.assertIn("[BaseModel] (123456)", bmstr)
-        self.assertIn("'id': '123456'", bmstr)
-        self.assertIn("'created_at': " + dt_repr, bmstr)
-        self.assertIn("'updated_at': " + dt_repr, bmstr)
+        time = datetime.utcnow()
+        time_repr = repr(time)
+        user1 = BaseModel()
+        user1.id = "123456"
+        user1.created_at = user1.updated_at = time
+        user1_str = user1.__str__()
+        self.assertIn("[BaseModel] (123456)", user1_str)
+        self.assertIn("'id': '123456'", user1_str)
+        self.assertIn("'created_at': " + time_repr, user1_str)
+        self.assertIn("'updated_at': " + time_repr, user1_str)
 
     def test_args_unused(self):
-        bm = BaseModel(None)
-        self.assertNotIn(None, bm.__dict__.values())
+        user1 = BaseModel(None)
+        self.assertNotIn(None, user1.__dict__.values())
 
     def test_instantiation_with_kwargs(self):
         dt = datetime.today()
@@ -78,12 +73,12 @@ class TestBaseModel_instantiation(unittest.TestCase):
             BaseModel(id=None, created_at=None, updated_at=None)
 
     def test_instantiation_with_args_and_kwargs(self):
-        dt = datetime.today()
-        dt_iso = dt.isoformat()
+        date_t = datetime.today()
+        dt_iso = date_t.isoformat()
         bm = BaseModel("12", id="345", created_at=dt_iso, updated_at=dt_iso)
         self.assertEqual(bm.id, "345")
-        self.assertEqual(bm.created_at, dt)
-        self.assertEqual(bm.updated_at, dt)
+        self.assertEqual(bm.created_at, date_t)
+        self.assertEqual(bm.updated_at, date_t)
 
 
 class TestBaseModel_save(unittest.TestCase):
